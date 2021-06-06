52f65ff65b23b82f57df8810ef2a4daa0bf47936
Joyson P S <joysonpathra@gmail.com>
2021-06-07 04:37:26 +0530

    removing token

--- GithubRepo/Utility/Constant.swift
+++ GithubRepo/Utility/Constant.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct Constants {
-    public static let github_token = "{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}"
+    public static let github_token = "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"
     public static let emptyLanguageError = "Please enter a programing language"
 }
 
